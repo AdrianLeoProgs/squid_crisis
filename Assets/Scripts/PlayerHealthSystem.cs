206fefacd5f9fb0a5303d0211c9eba3d8dddcd72
Nathan <Nathan.Argall@gmail.com>
2019-11-16 11:57:46 -0600

    health system scripts

---
+++ Assets/Scripts/PlayerHealthSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class PlayerHealthSystem : MonoBehaviour
+{
+    private static readonly object padlock = new object();
+    private static PlayerHealthSystem instance = null;
+    [SerializeField] private int _health;
+
+    // private constructor
+    private PlayerHealthSystem() {}
+
+    void Start() 
+    {
+        _health = 5;
+    }
+
+    public static PlayerHealthSystem getInstance() 
+    {
+        if (instance == null) 
+        {
+            // synchronized zone
+            lock (padlock)
+            {
+                if (instance == null) 
+                {
+                    instance = new PlayerHealthSystem();
+                } 
+            }
+        }
+
+        return instance;
+    }
+    
+    public int health
+    {
+        get
+        {
+            return _health;
+        }
+
+        set
+        {
+            _health = value;
+        }
+    }
+}
---
+++ Assets/Scripts/SquidMainHealthSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquidMainBodyHealthSystem : MonoBehaviour
+{
+    private static readonly object padlock = new object();
+    private static SquidMainBodyHealthSystem instance = null;
+    [SerializeField] private int _health;
+
+    // private constructor
+    private SquidMainBodyHealthSystem() {}
+
+    void Start()
+    {
+        _health = 100;
+    }
+
+    public static SquidMainBodyHealthSystem getInstance() 
+    {
+        if (instance == null)
+        {
+            // synchronized zone
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new SquidMainBodyHealthSystem();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public int health
+    {
+        get
+        {
+            return _health;
+        }
+
+        set
+        {
+            _health = value;
+        }
+    }
+}
