f98df217a25d5031d0cec37594e89310369d8732
Nathan <Nathan.Argall@gmail.com>
2019-11-16 16:42:28 -0600

    shooting:

--- Assets/Scripts/TurretScope.cs
+++ Assets/Scripts/TurretScope.cs
@@ -5,24 +5,42 @@ using Valve.VR;
 
 public class TurretScope : MonoBehaviour
 {
-
     public SteamVR_Action_Boolean triggerPulled;
 
     public SteamVR_Input_Sources hand;
 
+    private bool isSquidHit;
+
     void Start()
     {
+        isSquidHit = false;
         triggerPulled.AddOnStateDownListener(TriggerDown, hand);
         triggerPulled.AddOnStateUpListener(TriggerUp, hand);
     }
 
+    void FixedUpdate() 
+    {
+        if (isSquidHit)
+        {
+            GameManager.getInstance().squidMainHealth -= 1;
+        }
+    }
+
     public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        Debug.Log("Trigger is up");
+        isSquidHit = false;
     }
 
     public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        Debug.Log("Trigger is down");
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100) && hit.collider.gameObject.CompareTag("Squid"))
+        {
+            isSquidHit = true;
+        }
+        else
+        {
+            isSquidHit = false;
+        }
     }
 }
