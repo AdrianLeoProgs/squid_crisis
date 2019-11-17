d5f28cef0a63b55eac82f4a1d9fe5e02464700f0
Argall <argalln@ds.ad.adp.com>
2019-11-17 08:28:00 -0600

    take out SquidAction script and replace functionality within GameManager and Tentacle scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,8 @@ public class GameManager : MonoBehaviour
 
     public Animator animator;
 
+    public List<List<GameObject>> phaseOneTentacleSet;
+
     // Ensure thread safety for Game Manager
     public static GameManager getInstance
     {
@@ -86,19 +88,53 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void Awake() 
+    {
+        for (List<GameObject> tentacles : phaseOneTentacleSet)
+        {
+            for (tentacle : tentacles)
+            {
+                tentacle.SetEnabled(false);
+            }
+        }
+    }
+
     void Start()
     {
-        // Disable 2nd and 3rd phase squid phases
+        // Enable first phase
         animator.SetBool("PhaseOne", true);
     }
 
     void Update()
     {
+        // Remove tentacle list from tentacleSet if said list is empty (shift the "first" index)
+        if (phaseOneTentacleSet.First().Count < 1)
+        {
+            phaseOneTentacleSet.Remove(phaseOneTentacleSet.First());
+        }
+
+        // Always enable the first List of tentacles in tentacleSet (since list will change dynamically)
+        for (GameObject tentacle : phaseOneTentacleSet.First())
+        {
+            tentacle.SetEnabled(true);
+        }
+
+        // Check for tentacle set count of current phase (more can be added later)
+        if (animator.GetBool("PhaseOne") && phaseOneTentacleSet.Count < 1)
+        {
+            // For now since we are only doing one phase, killing all tentacles in this will beat the boss
+            GameManager.getInstance.squidMainHealth -= 100;
+            // "PhaseTwo" in this case can just be the boss end phase until we feel more confident we can add additional phases
+            squidPhase = SquidPhases.PHASE_TWO;
+        }
+
+        // Setting boss phases
         if (SquidPhases.PHASE_TWO.Equals(_squidPhase))
         {
             animator.SetBool("PhaseOne", false);
             animator.SetBool("PhaseTwo", true);
         }
+        // May never get to this point
         else if (SquidPhases.PHASE_THREE.Equals(_squidPhase))
         {
             animator.SetBool("PhaseTwo", false);
--- Assets/Scripts/Tentacle.cs
+++ Assets/Scripts/Tentacle.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class Tentacle : MonoBehaviour
 {
+    public List<GameObject> targets;
+
+    public Animator animator;
+
+    public String animatorBool;
+
+    void Start() 
+    {
+        // Set the animation
+        animator.SetBool(animatorBool, true);
+    }
+
+    void Update() 
+    {
+        // destroy self and transistion animation if all targets are destroyed
+        if (targets.Length() < 1)
+        {
+            StartCoroutine("AnimationTransition");   
+        }
+    }
+
     void OnCollisionEnter(Collision other) 
     {
         if (other.gameObject.CompareTag("Player"))
         {
             GameManager.getInstance.playerHealth -= 1;
         }
+    }
+
+    IEnumertor AnimationTransition()
+    {
+        animator.SetBool(animatorBool, false);
+        // yield wait can be altered to diff time when we figure out about how long the end animation usually takes
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject, 2);
     }
 
 }
