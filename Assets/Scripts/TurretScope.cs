d7e41554e0ed2d0ba38c2c8a566763b6257967af
Adrian Leonard <leonard.adrian96@gmail.com>
2019-11-17 18:46:12 -0600

    Machine gun sounds

--- Assets/Scripts/TurretScope.cs
+++ Assets/Scripts/TurretScope.cs
@@ -6,7 +6,7 @@ using Valve.VR;
 public class TurretScope : MonoBehaviour
 {
     public SteamVR_Action_Boolean triggerPulled;
-    
+
     public SteamVR_Action_Vibration vibration;
 
     public SteamVR_Input_Sources hand;
@@ -16,7 +16,7 @@ public class TurretScope : MonoBehaviour
     public float fireRate = 15f;
 
     public float nextTimeToFire = 0f;
-    
+
     public GameObject waterSplash;
 
     public GameObject targetHitSplash;
@@ -35,61 +35,59 @@ public class TurretScope : MonoBehaviour
     void Start()
     {
         triggerPulled.AddOnStateDownListener(TriggerDown, hand);
-        triggerPulled.AddOnStateUpListener(TriggerUp, hand);       
+        triggerPulled.AddOnStateUpListener(TriggerUp, hand);
     }
 
-    void FixedUpdate() 
+    void FixedUpdate()
     {
-        if (isShooting)
+
+        if (isShooting && Time.time >= nextTimeToFire)
         {
-            machineGunRapid.Play();
-            if (Time.time >= nextTimeToFire)
+
+            vibration.Execute(0, 1, 320, 1, hand);
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Vector3 forward = transform.TransformDirection(Vector3.back) * 100;
+            Debug.DrawRay(transform.position, forward, Color.green);
+            Instantiate(muzzleflashEffect, muzzlePivot.position, muzzlePivot.rotation);
+            Instantiate(smokeEffect, smokePivot.position, smokePivot.rotation);
+            RaycastHit hit;
+            bool raycasthit = Physics.Raycast(transform.position, forward, out hit, 100);
+            if (raycasthit)
             {
-
-                vibration.Execute(0, 1, 320, 1, hand);
-                nextTimeToFire = Time.time + 1f / fireRate;
-                Vector3 forward = transform.TransformDirection(Vector3.back) * 100;
-                Debug.DrawRay(transform.position, forward, Color.green);
-                GameObject muzzleFlash = Instantiate(muzzleflashEffect, muzzlePivot.position, muzzlePivot.rotation);
-                GameObject smokeFlash = Instantiate(smokeEffect, smokePivot.position, smokePivot.rotation);
-                Destroy(muzzleFlash);
-                Destroy(smokeFlash);
-                RaycastHit hit;
-                bool raycasthit = Physics.Raycast(transform.position, forward, out hit, 100);
-                if (raycasthit)
+                GameObject hitObject = hit.collider.gameObject;
+                if (hitObject.CompareTag("Target"))
                 {
-                    GameObject hitObject = hit.collider.gameObject;
-                    if (hitObject.CompareTag("Target"))
-                    {
-                        // destroy target instance
-                        GameManager.getInstance.removeTarget(hitObject);
-                        GameObject targetHitSplash = Instantiate(this.targetHitSplash, hit.point, Quaternion.LookRotation(hit.normal));
-                        Destroy(targetHitSplash, 2f);
-                    }
-                    else if (hitObject.CompareTag("Water"))
-                    {
-                        GameObject waterSplashes = Instantiate(waterSplash, hit.point, Quaternion.LookRotation(hit.normal));
-                        Destroy(waterSplashes, 2f);
-                    }
-                    else if (hitObject.CompareTag("Squid"))
-                    {
-                        print("Hit squid body");
-                        GameObject bodySplashes = Instantiate(bodyHitSplash, hit.point, Quaternion.LookRotation(hit.normal));
-                        Destroy(bodySplashes, 2f);
-                    }
+                    // destroy target instance
+                    GameManager.getInstance.removeTarget(hitObject);
+                    GameObject targetHitSplash = Instantiate(this.targetHitSplash, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(targetHitSplash, 2f);
                 }
-
+                else if (hitObject.CompareTag("Water"))
+                {
+                    GameObject waterSplashes = Instantiate(waterSplash, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(waterSplashes, 2f);
+                }
+                else if (hitObject.CompareTag("Squid"))
+                {
+                    print("Hit squid body");
+                    GameObject bodySplashes = Instantiate(bodyHitSplash, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(bodySplashes, 2f);
+                }
             }
+
+
         }
     }
 
     public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
         isShooting = false;
+        machineGunRapid.Stop();
     }
 
     public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
         isShooting = true;
+        machineGunRapid.Play();
     }
 }
